48a06782faf86715f8129d89a0083b68bc8fac44
Mark Whitehead <mjamesw2010@gmail.com>
2023-03-06 19:44:48 +0000

    created an add place form'

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import Input from '../../shared/components/FormElements/Input';
+
+import './NewPlace.css';
 
 function NewPlace() {
-  return <h2>NewPLace works</h2>;
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 }
 export default NewPlace;
