a20838827e0c436e654c679911490d5baa2a28de
Mark Whitehead <mjamesw2010@gmail.com>
2023-04-04 22:57:40 +0100

    Sharing Input Values and Adding Multiple State

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/Util/validators';
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from '../../shared/Util/validators';
 import './NewPlace.css';
 
 function NewPlace() {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
   return (
     <form className="place-form">
       <Input
@@ -12,6 +18,15 @@ function NewPlace() {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
