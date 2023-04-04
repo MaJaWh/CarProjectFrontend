3015527b4a104ee93455f654e0de12b3de68cec9
Mark Whitehead <mjamesw2010@gmail.com>
2023-04-04 21:01:08 +0100

    Implemented form validation that shows an error if no text is entered in the field ADD PLACE

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Input from '../../shared/components/FormElements/Input';
-
+import { VALIDATOR_REQUIRE } from '../../shared/Util/validators';
 import './NewPlace.css';
 
 function NewPlace() {
@@ -10,7 +10,7 @@ function NewPlace() {
         element="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
       />
     </form>
